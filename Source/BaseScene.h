17fd9cb6f5651f3b32a0f137ffc89ea46ec74e07
arkPenguin <2210337@ecc.ac.jp>
2022-07-12 16:32:18 +0900

    Clear画面作成+α
    ・クリア画面をだいたい作成。
    ・2D画像描画クラスとUI選択系クラスをBaseScene.hに移動。
    ・上記の関数定義のためBaseScene.cppを作成。あと改良。
    ・SceneManagerにSceneClear用処理追加

--- Source/BaseScene.h
+++ Source/BaseScene.h
@@ -6,6 +6,8 @@
 * -Attention    - Nothing -
 */
 
+#include <DirectXMath.h>
+#include <vector>
 
 enum class SCENE_TYPE;
 class Scene
@@ -24,4 +26,121 @@ public:
     /* get type */
     virtual const SCENE_TYPE getType() = 0;
 };
+
+//------------------------//
+
+#include "Graphics/Sprite.h"
+enum class SPRITE_PIVOT {
+    LT,CT,RT
+    ,LM,CM,RM
+    ,LB,CB,RB
+};
+class Texture :public Sprite
+{
+public:
+    // constructer etc.
+    Texture(
+        const char* filename
+        , const DirectX::XMFLOAT2& pos = {}
+        , SPRITE_PIVOT pivot = SPRITE_PIVOT::LT
+        , const DirectX::XMFLOAT2& scale = { 1,1 }
+        , int zCode = 0
+        , const DirectX::XMFLOAT2& texPos = {}
+        , const DirectX::XMFLOAT2& texSize = {}
+        , float angle = 0.0f
+        , const DirectX::XMFLOAT4& color = { 1,1,1,1 }
+    );
+    Texture(Texture&) = delete;
+
+    /* main method */
+    /// <param name="p">基準点</param>
+    void Render(const DirectX::XMFLOAT2& p);
+
+    //getter
+    const DirectX::XMFLOAT2& GetPosition()const { return position_; }
+    const DirectX::XMFLOAT2& GetScale()const { return scale_; }
+    const DirectX::XMFLOAT2& GetTexPos()const { return texPos_; }
+    const DirectX::XMFLOAT2& GetTexSize()const { return texSize_; }
+    float GetAngle()const { return angle_; }
+    const DirectX::XMFLOAT4& GetColor()const { return color_; }
+    int GetZ()const { return zCode_; }
+
+    //setter
+    void SetScale(const DirectX::XMFLOAT2& s) { scale_ = s; }
+    void SetColor(const DirectX::XMFLOAT4& c) { color_ = c; }
+    void SetAngle(float a) { angle_ = normalizeAngle(a); }
+    void AddAngle(float a) { normalizeAngle(angle_ += a); }
+
+private:
+    DirectX::XMFLOAT2 position_{};
+    SPRITE_PIVOT pivot_{};
+    DirectX::XMFLOAT2 scale_{};
+    DirectX::XMFLOAT2 texPos_{};
+    DirectX::XMFLOAT2 texSize_{};
+    float angle_{};
+    DirectX::XMFLOAT4 color_{};
+    int zCode_ = 0;
+private:
+    inline float normalizeAngle(float& angle)
+    {
+        if (angle < -DirectX::XM_PI) angle += DirectX::XM_2PI;
+        if (DirectX::XM_PI < angle) angle -= DirectX::XM_2PI;
+        return angle;
+    }
+};
+
+class MenuBar
+{
+public:
+    //constructer etc.
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="size">矩形整列用</param>
+    /// <param name="vertical">矩形整列時上下にバー移動できるか否か</param>
+    /// <param name="horizontal">矩形整列時左右限界を移動できるか否か</param>
+    MenuBar(const DirectX::XMFLOAT2& size, bool loop = true, bool horizontal = true, bool vertical = true)
+        :loop_(loop), size_(size), moveVertical_(vertical), moveHorizontal_(horizontal)
+    {}
+    ~MenuBar() {
+        if (logo_) delete logo_;
+        if (BG_) delete BG_;
+        for (auto it : subBar_)
+        {
+            if (it != nullptr)
+                delete it;
+        }
+        subBar_.clear();
+    }
+
+    /* main method */
+    void Update();
+    void Render();
+
+    //setter
+    void SetLogo(Texture* s) { logo_ = s; }
+    void SetBG(Texture* s) { BG_ = s; }
+    void SetPosition(const DirectX::XMFLOAT2& p) { position_ = p; }
+    //
+    void AddSubBer(Texture* s, void* func) { subBar_.emplace_back(s); func_.emplace_back(func); }
+    void AddTexture(Texture* s) { textures_.emplace_back(s); }
+private:
+    DirectX::XMFLOAT2 position_ = {};
+
+    Texture* logo_ = nullptr;
+    Texture* BG_ = nullptr;
+    std::vector<Texture*> textures_{};
+    std::vector<Texture*> subBar_{};
+    std::vector<void*> func_{}; //Bar毎の処理格納庫
+
+    bool loop_ = false;     // 限界を超える操作をしたときに反対の限界値にいくか
+    int index_ = 0;         // 選択中のバー番号
+    DirectX::XMFLOAT2 size_ = { 1,1 };  // バーの行列数
+    bool moveVertical_ = false;         // 矩形整列時など選択バーの上下移動可不可
+    bool moveHorizontal_ = false;       // 矩形整列時など選択バーの左右限界移動可不可
+    bool sortHorizontal_ = false;       // バー並べの優先方向。未使用。面倒くさいから描画位置で誤魔化してちょ
+    bool islarge_ = false;  // 今は使ってない。バーのｴﾌｪｸﾄｫｫｵ用だけどやる気ナス
+
+};
+
 
--- Source/SceneTitle.cpp
+++ Source/SceneTitle.cpp
@@ -4,142 +4,6 @@
 #include "Input/Input.h"
 
 
-TitleSprite::TitleSprite(const char* filename, const DirectX::XMFLOAT2& pos, SPRITE_PIVOT pivot, const DirectX::XMFLOAT2& scale, const DirectX::XMFLOAT2& texPos, const DirectX::XMFLOAT2& texSize, float angle, const DirectX::XMFLOAT4& color)
-    :Sprite(filename)
-{
-    position_ = pos;
-    pivot_ = pivot;
-    scale_ = scale;
-    texPos_ = texPos;
-    if (texSize.x * texSize.y == 0.0f)
-        texSize_ = { static_cast<float>(GetTextureWidth()),static_cast<float>(GetTextureHeight()) };
-    else
-        texSize_ = texSize;
-    angle_ = angle;
-    color_ = color;
-}
-
-void TitleSprite::Render()
-{
-    auto dc = Graphics::Instance().GetDeviceContext();
-
-    auto pos = GetPosition();
-    switch (pivot_)
-    {
-    case SPRITE_PIVOT::LT: /*nothing*/ break;
-    case SPRITE_PIVOT::CT: pos.x -= texSize_.x * scale_.x / 2;                                      break;
-    case SPRITE_PIVOT::RT: pos.x -= texSize_.x * scale_.x;                                          break;
-    case SPRITE_PIVOT::LM:                                      pos.y -= texSize_.y * scale_.y / 2; break;
-    case SPRITE_PIVOT::CM: pos.x -= texSize_.x * scale_.x / 2,  pos.y -= texSize_.y * scale_.y / 2; break;
-    case SPRITE_PIVOT::RM: pos.x -= texSize_.x * scale_.x,      pos.y -= texSize_.y * scale_.y / 2; break;
-    case SPRITE_PIVOT::LB:                                      pos.y -= texSize_.y * scale_.y;     break;
-    case SPRITE_PIVOT::CB: pos.x -= texSize_.x * scale_.x / 2,  pos.y -= texSize_.y * scale_.y;     break;
-    case SPRITE_PIVOT::RB: pos.x -= texSize_.x * scale_.x,      pos.y -= texSize_.y * scale_.y;     break;
-    }
-
-    Sprite::Render(dc
-        , pos.x, pos.y
-        , texSize_.x * scale_.x, texSize_.y * scale_.y
-        , texPos_.x, texPos_.y
-        , texSize_.x, texSize_.y
-        , angle_
-        , color_.x, color_.y, color_.z, color_.w
-    );
-}
-
-//---------------------------------//
-//---------------------------------//
-//---------------------------------//
-
-void MenuBar::Update() {
-    // change scene
-    {
-        auto& input = Input::Instance().GetGamePad();
-
-        //Bar選択
-#if 1
-        //AXIS LY
-        {
-            static bool up = false;
-            const auto& axis = input.GetAxisLY();
-
-            if (up == false) //trigger
-                if (axis != 0.0f)
-                {
-                    //増減
-                    if (axis <= -0.75f)
-                        --index_;
-                    else if (0.75f <= axis)
-                        ++index_;
-
-                    //clamp制御
-                    if (loop_)
-                    {
-                        if (index_ < 0) index_ = subBar_.size() - 1;
-                        if (subBar_.size() - 1 < index_) index_ = 0;
-                    }
-                    else index_ = (std::min)((std::max)(0, index_), static_cast<int>(subBar_.size()) - 1);
-                }
-            up = (axis != 0.0f);
-        }
-#else
-        //BUTTON
-        {
-            switch ((input.GetButtonDown() & (GamePad::BTN_UP | GamePad::BTN_DOWN)))
-            {
-            case (GamePad::BTN_UP):
-                --index_;
-                if (index_ < 0)
-                    index_ = (loop_) ? subBar_.size() - 1 : 0; //loopオンなら一番下にする。オフなら最高値にする。
-                break;
-            case (GamePad::BTN_DOWN):
-                ++index_;
-                if (subBar_.size() <= index_)
-                    index_ = (loop_) ? 0 : subBar_.size() - 1; //loopオンなら一番上にする。
-                break;
-            }
-        }
-#endif
-
-        //change
-        if (Input::Instance().GetGamePad().GetButtonDown()
-            & GamePad::BTN_START)
-            func_[index_];
-    }
-
-    // bar update
-    {
-        //はいぷろ見ながらつくる
-        //subBar_[index_]->SetScale()
-        for (auto& bar : subBar_)
-        {
-            bar->SetColor({ 0.70f,0.70f,0.70f,1 });
-            if (bar == subBar_.at(index_)) bar->SetColor({ 1,1,1,1 });
-        }
-    }
-}
-
-void MenuBar::Render()
-{
-    auto dc = Graphics::Instance().GetDeviceContext();
-
-    // BG
-    if (BG_ != nullptr)
-        BG_->Render();
-
-    // subBar_
-    for (auto& it : subBar_) 
-        it->Render();
-
-    // titleLogo_
-    if (titleLogo_ != nullptr)
-        titleLogo_->Render();
-}
-
-//---------------------------------//
-//---------------------------------//
-//---------------------------------//
-
 /* main method */
 
 inline void apa() {
@@ -153,27 +17,30 @@ void SceneTitle::Initialize()
 {
     const DirectX::XMFLOAT2 screen = { Graphics::Instance().GetScreenWidth(),Graphics::Instance().GetScreenHeight() };
 
-    menu_ = std::make_unique<MenuBar>(new MenuBar(true));
+    menu_ = std::make_unique<MenuBar>(DirectX::XMFLOAT2(1,2));
 
     //↓仮設
-    menu_->SetBG(new TitleSprite(".\\Data\\Sprite\\Title.png"));
-    menu_->SetTitleLogo(new TitleSprite(
+    menu_->SetBG(new Texture(".\\Data\\Sprite\\Title.png"));
+    menu_->SetLogo(new Texture(
         ".\\Data\\Sprite\\LoadingIcon.png"
         , { screen.x / 2,screen.y / 4 }
         , SPRITE_PIVOT::CM
     ));
-    menu_->AddSubBer(new TitleSprite(
+    menu_->AddSubBer(new Texture(
         ".\\Data\\Sprite\\Title.png"
         , { screen.x / 2,screen.y * 3 / 5 }
         , SPRITE_PIVOT::CM
         , { 0.5f,0.5f }
     ), apa);
-    menu_->AddSubBer(new TitleSprite(
+    menu_->AddSubBer(new Texture(
         ".\\Data\\Sprite\\Title.png"
         , { screen.x / 2,screen.y * 4 / 5 }
         , SPRITE_PIVOT::CM
         , { 0.5f,0.5f }
     ), apue);
+    menu_->AddTexture(new Texture(
+        "C:\\Users\\2210337\\Desktop\\iPhoneより\\IMG_5828.JPG"
+    ));
 }
 
 void SceneTitle::Finalize()
